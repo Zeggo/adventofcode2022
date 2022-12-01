41af6fe43e379c524147638e4c08677745a35d9a
Zeggo <5673364+Zeggo@users.noreply.github.com>
2022-12-01 23:20:03 +0200

    Part 2 done, tidying up Part 1

--- 01/01.py
+++ 01/01.py
@@ -14,11 +14,16 @@ for line in input:
         elves.append(calories)
         calories = 0        
 
-    # continue?
-    # print(line)
+# Part 1: Compare values in list, find biggest value (max)
+print("The elf carrying the most calories is carrying", max(elves), "Calories.")
 
-print(max(elves))
+# Part 2: Find the top three elves carrying the most Calories, count the total
 
-# compare values in list, find biggest value (max)
+# sort the list, find top three, calculate the sum
+elves.sort(reverse=True)
+total_calories_of_top_elves = sum(elves[0:3])
+
+print("The top three elves are carrying", elves[0], "and", elves[1], "and", elves[2], "Calories on them.")
+print("In total, they are carrying", total_calories_of_top_elves, "Calories.")
 
 input.close()
