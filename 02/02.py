85a374c116b6196690b8a407ee16dc5082d07ef8
Zeggo <5673364+Zeggo@users.noreply.github.com>
2022-12-02 19:19:51 +0200

    create 02 files, start defining

---
+++ 02/02.py
@@ -0,0 +1,15 @@
+# Day 2: Rock Paper Scissors
+
+# Points:
+# rock     X => 1
+# paper    Y => 2
+# scissors Z => 3
+
+# open file
+input = open(r"02/input2.txt", encoding="utf-8")
+# test file
+for line in input:
+    # print(line)
+
+# close file
+input.close()
