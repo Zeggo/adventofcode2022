a08578504f5d8f440a34ff069c83c4ef109ae237
Zeggo <5673364+Zeggo@users.noreply.github.com>
2022-12-03 11:42:43 +0200

    refactoring: find_prio function

--- 03/03.py
+++ 03/03.py
@@ -10,10 +10,15 @@ calculate sum of priorities
 
 #input = open(r"03/input3.txt", encoding="utf-8")
 input = ["vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "PmmdzqPrVvPwwTWBwg", "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", "ttgJtRGJQctTZtZT", "CrZsJsPPZsGzwwsLwLmpwMDw"]
-itemtypes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-itemtype = ""
 
 prio_sum = 0
+
+def find_prio(itemtype):
+    itemtypes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    
+    for i in range(len(itemtypes)):
+        if itemtype == itemtypes[i]:
+            return i+1
 
 for line in input:
 
@@ -32,16 +37,12 @@ for line in input:
 
             if item1 == item2:
                 found = True
-                itemtype = item1
+                prio_sum += find_prio(item1)
+                #itemtype = item1
                 #print(itemtype)
                 break
 
         if found == True:
-            break
-
-    for i in range(len(itemtypes)):
-        if itemtype == itemtypes[i]:
-            prio_sum += i+1
             break
 
 print("Part 1:", prio_sum)
@@ -49,7 +50,6 @@ print("Part 1:", prio_sum)
 # Part 2
 # compare three consecutive lines to find common badge item (letter)
 # calculate sum of badge priorities
-badge = ""
 badge_sum = 0
 
 for i in range(0, len(input), 3):
