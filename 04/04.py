e17a8eeb201ce115d86a72f8e24daca8a52f0a87
Zeggo <5673364+Zeggo@users.noreply.github.com>
2022-12-04 09:37:23 +0200

    Day 4 part 2 solution, find all overlaps

--- 04/04.py
+++ 04/04.py
@@ -14,6 +14,7 @@ input_file.close()
 #input = ["2-4,6-8", "2-3,4-5", "5-7,7-9", "2-8,3-7", "6-6,4-6", "2-6,4-8"] #test input
 
 duplicate_assignments = 0
+overlapping_assignments = 0
 
 for line in input:
     # Splitting the input into a list
@@ -30,4 +31,8 @@ for line in input:
     if ass1.issubset(ass2) or ass2.issubset(ass1):
         duplicate_assignments += 1
 
+    if not(ass1.isdisjoint(ass2)):
+        overlapping_assignments +=1
+
 print(duplicate_assignments)
+print(overlapping_assignments)
