72ded02166da7d306fccc4692b8ebaf3b4b70430
Zeggo <5673364+Zeggo@users.noreply.github.com>
2022-12-05 18:34:58 +0200

    procedure as numbers, stacks with top crates

--- 05/05.py
+++ 05/05.py
@@ -5,6 +5,7 @@
 
 input_file = open(r"05/input5.txt", encoding="utf-8")
 
+# Split the input into two
 crates = []
 procedure = []
 split = False
@@ -16,13 +17,23 @@ for line in input_file:
     if split == False:
         crates.append(line)
     if split == True:
-        procedure.append(line.split( )) # Make a list that includes lists with the procedure numbers in indexes 1, 3 and 5
+        procedure.append([int(i) for i in line.split() if i.isnumeric()]) # Make a list that includes lists with the procedure numbers
 
 input_file.close()
 
-#print(crates)
+print(crates[0])
 #print("\n\n\nBREAK HERE\n\n\n")
-print(procedure)
+#print(procedure)
 
-numbers = [i for i in input if i.isnumeric()]
-#print(numbers)
+# Columns to rows => lists
+# columns in same places -> placement to list name
+# row -> placement in list, at the end => add new items to [0], not replacing previous items
+
+# Take a symbol every x characters in string and add to list[0] if it is whitespace, ignore
+stacks = list(([],[],[],[],[],[],[],[],[]))
+stack_index = 0
+for index in range(1, len(crates[0]), 4):
+    stacks[stack_index].append(crates[0][index])
+    stack_index += 1
+
+print(stacks)
