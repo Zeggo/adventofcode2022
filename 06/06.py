5fe6b08bcefd7d6f0ea44a0db127d90bccb360a3
Zeggo <5673364+Zeggo@users.noreply.github.com>
2022-12-06 11:46:57 +0200

    Refactored into a function, updated comments

--- 06/06.py
+++ 06/06.py
@@ -1,25 +1,17 @@
 # Day 6: Tuning Trouble
-# From the datastream, find the marker: four different characters in a row
+# From the datastream, find the marker: four/fourteen different characters in a row
 # Return the value where the four different characters have been received for the first time
 input_file = open(r"06/input6.txt", encoding="utf-8")
 input = input_file.read()
 input_file.close()
 
+def find_unique_set(set_length, input):
+    last_set = ""
+    for char in range(set_length, len(input)):
+        last_set = input[char - set_length:char]
+        #print(last_set)
+        if len(set(last_set)) == len(last_set):
+            return char
 
-# Part 1: Find the start-of-packet marker with four distinct characters
-last_four = ""
-for char in range(4, len(input)):
-    last_four = input[char-4:char]
-    #print(last_four)
-    if len(set(last_four)) == len(last_four):
-        print("Part 1: The start-of-packet marker is at location", char)
-        break
-
-# Part 2: Find the start-of-message marker with fourteen distinct characters
-last_fourteen = ""
-for char in range(14, len(input)):
-    last_fourteen = input[char-14:char]
-    #print(last_fourteen)
-    if len(set(last_fourteen)) == len(last_fourteen):
-        print("Part 2: The start-of-message marker is at location", char)
-        break
+print("Part 1: The start-of-packet marker is at location", find_unique_set(4, input))
+print("Part 2: The start-of-message marker is at location", find_unique_set(14, input))
