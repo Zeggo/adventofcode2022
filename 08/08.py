997ce3f6630d5e16c7e06d7c62df8ca6b7da0620
Zeggo <5673364+Zeggo@users.noreply.github.com>
2022-12-09 00:44:28 +0200

    Debugging, still error in last inner tree

--- 08/08.py
+++ 08/08.py
@@ -11,35 +11,76 @@ test_input = """30373
 35390"""
 
 input = test_input.split("\n")
-print(input)
+#print(input)
 
 visible_trees = 0
 
 for y in range(len(input)):
     for x in range(len(input[y])):
-        # edge trees:
+        direction = []
+
+        # Edge trees:
         if y == 0 or y == len(input)-1 or x == 0 or x == len(input[y])-1:
             visible_trees += 1
-        # interior trees:
+            #print("tree at", y, x, "is visible")
+        
+        # Interior trees:
         else:
             is_visible = False
-            # check left
+            # Check left
             for i in range(len(input[y][:x])):
                 if input[y][x] > input[y][i]:
                     is_visible = True
-                    break
-            # check right
-            for i in range(len(input[y][x+1:])):
-                if input[y][x] > input[y][i]:
-                    is_visible = True
-                    break
-            # check up
-            for i in range(len(input[:y])):
-                #print(input[i])
-                pass
+                else: is_visible = False
+            if is_visible == True: direction.append("left")
 
-            # check down
+            if is_visible == False:    
+                # Check right
+                for i in range(x, len(input[y][x+1:])):
+                    if input[y][x] > input[y][i]:
+                        is_visible = True
+                    else: is_visible = False
+                if is_visible == True: direction.append("right")
+
+                if is_visible == False:        
+                    # Check up
+                    for i in range(len(input[:y])):
+                        if input[y][x] > input[i][x]:
+                            is_visible = True
+                        else: is_visible = False
+                    if is_visible == True: direction.append("up")
+                    
+                    if is_visible == False:
+                        # Check down
+                        for i in range(y, len(input[y+1:])):
+                            if input[y][x] > input[i][x]:
+                                is_visible = True
+                            else: is_visible = False
+                        if is_visible == True: direction.append("down")
 
             if is_visible == True:
                 visible_trees += 1
-    print(visible_trees)
+                print("tree at", y, x, "is visible from", direction)
+
+        #print("trees when x =", x, "is:", visible_trees)
+    #print("at y =", y, "there are", visible_trees)
+
+print("Total visible trees:", visible_trees)
+
+"""
+y = 3
+x = 1
+
+check up:
+[0][1] => y-3, x
+[1][1]
+[2][1]
+
+
+
+
+check down:
+input[2:][1]
+
+error in y = 3, x = 1 or 3
+"""
